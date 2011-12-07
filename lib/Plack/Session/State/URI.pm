928baf102441d71ca1e359a448f3969e32b6eb71
s-aska <s.aska.org@gmail.com>
2011-12-07 19:43:50 +0900

    initial commit

---
+++ lib/Plack/Session/State/URI.pm
@@ -0,0 +1,32 @@
+package Plack::Session::State::URI;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Plack::Session::State::URI -
+
+=head1 SYNOPSIS
+
+  use Plack::Session::State::URI;
+
+=head1 DESCRIPTION
+
+Plack::Session::State::URI is
+
+=head1 AUTHOR
+
+Shinichiro Aska E<lt>s.aska.org@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
